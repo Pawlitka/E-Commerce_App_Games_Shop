b71639e1fa932032d1e1cfa8f1f9acefe313210b
pawelkapusta <pawelllosek@gmail.com>
2025-08-01 16:26:35 +0200

    Fix HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,84 +1,72 @@
 <template>
   <div class="home">
-    <TopGamesCarousel
-        :slides="carouselSlides"
-    />
+    <TopGamesCarousel :slides="carouselSlides" />
   </div>
 </template>
-<script>
+
+<script setup>
 import TopGamesCarousel from "../components/TopGamesCarousel.vue";
-export default {
-  name: 'HomeView',
-  components: {TopGamesCarousel},
-  setup() {
-    // const carouselSlides = [
-    //     ['Cyberpunk2077.jpg','TombRaider.jpg','Witcher_3.jpg'],
-    //     ['Cyberpunk2077.jpg','Cyberpunk2077.jpg','Cyberpunk2077.jpg'],
-    //     ['TombRaider.jpg','TombRaider.jpg'],
-    //     ['Witcher_3.jpg','Witcher_3.jpg']
-    // ];
-    const carouselSlides = [
-      [
-        {
-          imagePath: 'Cyberpunk2077.jpg',
-          alternativeText: 'Image of game Cyberpunk 2077.',
-        },
-        {
-          imagePath: 'TombRaider.jpg',
-          alternativeText: 'Image of game Tomb Raider.'
-        },
-        {
-          imagePath: 'Witcher_3.jpg',
-          alternativeText: 'Image of game Witcher 3.'
-        }
-      ],
-      [
-        {
-          imagePath: 'TombRaider.jpg',
-          alternativeText: 'Image of game Tomb Raider.'
-        },
-        {
-          imagePath: 'Witcher_3.jpg',
-          alternativeText: 'Image of game Witcher 3.'
-        },
-        {
-          imagePath: 'Cyberpunk2077.jpg',
-          alternativeText: 'Image of game Cyberpunk 2077.'
-        }
-      ],
-          [
-          {
-            imagePath: 'Cyberpunk2077.jpg',
-            alternativeText: 'Image of game Cyberpunk 2077.',
-          },
-              {
-                imagePath: 'TombRaider.jpg',
-                alternativeText: 'Image of game Tomb Raider.'
-              },
-              {
-                imagePath: 'Witcher_3.jpg',
-                alternativeText: 'Image of game Witcher 3.'
-              }
-          ],
-      [
-        {
-          imagePath: 'TombRaider.jpg',
-          alternativeText: 'Image of game Tomb Raider.'
-        },
-        {
-          imagePath: 'Witcher_3.jpg',
-          alternativeText: 'Image of game Witcher 3.'
-        },
-        {
-          imagePath: 'Cyberpunk2077.jpg',
-          alternativeText: 'Image of game Cyberpunk 2077.'
-        }
-      ]
-    ];
-    return { carouselSlides };
-  }
-}
+
+const carouselSlides = [
+  [
+    {
+      imagePath: "Cyberpunk2077.jpg",
+      alternativeText: "Image of game Cyberpunk 2077.",
+    },
+    {
+      imagePath: "TombRaider.jpg",
+      alternativeText: "Image of game Tomb Raider.",
+    },
+    {
+      imagePath: "Witcher_3.jpg",
+      alternativeText: "Image of game Witcher 3.",
+    },
+  ],
+  [
+    {
+      imagePath: "TombRaider.jpg",
+      alternativeText: "Image of game Tomb Raider.",
+    },
+    {
+      imagePath: "Witcher_3.jpg",
+      alternativeText: "Image of game Witcher 3.",
+    },
+    {
+      imagePath: "Cyberpunk2077.jpg",
+      alternativeText: "Image of game Cyberpunk 2077.",
+    },
+  ],
+  [
+    {
+      imagePath: "Cyberpunk2077.jpg",
+      alternativeText: "Image of game Cyberpunk 2077.",
+    },
+    {
+      imagePath: "TombRaider.jpg",
+      alternativeText: "Image of game Tomb Raider.",
+    },
+    {
+      imagePath: "Witcher_3.jpg",
+      alternativeText: "Image of game Witcher 3.",
+    },
+  ],
+  [
+    {
+      imagePath: "TombRaider.jpg",
+      alternativeText: "Image of game Tomb Raider.",
+    },
+    {
+      imagePath: "Witcher_3.jpg",
+      alternativeText: "Image of game Witcher 3.",
+    },
+    {
+      imagePath: "Cyberpunk2077.jpg",
+      alternativeText: "Image of game Cyberpunk 2077.",
+    },
+  ],
+];
 </script>
+
 <style lang="scss" scoped>
 .home {
   height: 100%;
