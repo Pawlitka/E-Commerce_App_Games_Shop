53d8468d52e6968aab582d8c7c94b7cd86ce8137
pawelkapusta <pawelllosek@gmail.com>
2025-08-08 23:51:40 +0200

    add reviews and correct cart section

--- src/components/GameTile.vue
+++ src/components/GameTile.vue
@@ -19,14 +19,14 @@ const props = defineProps({
       :src="require('@/assets/Cyberpunk2077.jpg')"
       :alt="gameTileTitle"
     />
-    <div :class="$style['game-tile__content']">
-      <div :class="$style['game-tile__title']">CYBERPUNK 2077</div>
-      <div :class="$style['game-tile__genres']">
+    <div :class="[$style.content, $style['game-tile__content']]">
+      <div :class="$style['content__title']">CYBERPUNK 2077</div>
+      <div :class="$style['content__genres']">
         <template v-for="index in genres" :key="index">
-          <div :class="$style['game-tile__genre']">{{ index.name }}</div>
+          <div :class="$style['content__genre']">{{ index.name }}</div>
         </template>
       </div>
-      <div :class="$style['game-tile__description']">
+      <div :class="$style['content__description']">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
         imperdiet aliquet urna, nec posuere dui efficitur in. Nam cursus dapibus
         est, quis laoreet tellus viverra nec. Fusce nec libero enim. Vestibulum
@@ -36,11 +36,19 @@ const props = defineProps({
     <div :class="[$style.cart, $style['game-tile__cart']]">
       <h1 :class="[$style['cart__price']]">PLN 159.99</h1>
       <div :class="$style['cart__rate']">
-        <img
-          :class="$style['cart__rate--icon']"
-          :src="require('@/assets/GameTile/star.png')"
-          alt="Star icon"
-        />
+        <div :class="$style['cart__rate--stars']">
+          <img
+            :class="$style['cart__rate--icon']"
+            :src="require('@/assets/GameTile/star.png')"
+            alt="Star icon"
+          />
+          <img
+            :class="$style['cart__rate--icon']"
+            :src="require('@/assets/GameTile/star.png')"
+            alt="Star icon"
+          />
+        </div>
+        <h1 :class="$style['cart__rate--reviews']">2137 reviews</h1>
       </div>
       <div :class="$style['cart__button']">
         <button type="button" :class="$style['cart__button--default']">
@@ -90,79 +98,94 @@ const props = defineProps({
     align-items: center;
   }
 }
-.game-tile__content--cart {
-  width: 300px;
-  height: 100%;
-  display: flex;
-  justify-content: flex-end;
-  align-items: self-end;
-}
 
-.game-tile__title {
-  width: 100%;
-  display: flex;
-  justify-content: start;
-  padding: 20px 0 0 20px;
-  font-weight: bold;
-  font-size: 2rem;
-  font-family: Inter, serif;
-}
+.content {
+  &__title {
+    width: 100%;
+    display: flex;
+    justify-content: start;
+    padding: 20px 0 0 20px;
+    font-weight: bold;
+    font-size: 2rem;
+    font-family: Inter, serif;
+  }
 
-.game-tile__genres {
-  width: 100%;
-  height: 33px;
-  padding: 10px 10px 20px 20px;
-  gap: 10px;
-  flex-direction: row;
-  display: flex;
-}
+  &__genres {
+    width: 100%;
+    height: 33px;
+    padding: 10px 10px 20px 20px;
+    gap: 10px;
+    flex-direction: row;
+    display: flex;
+  }
 
-.game-tile__genre {
-  width: 130px;
-  height: 30px;
-  display: flex;
-  background: #c2a875;
-  color: #f5f5f5;
-  justify-content: center;
-  align-items: center;
-  border-radius: 10px;
-}
+  &__genre {
+    width: 130px;
+    height: 30px;
+    display: flex;
+    background: #c2a875;
+    color: #f5f5f5;
+    justify-content: center;
+    align-items: center;
+    border-radius: 10px;
+  }
 
-.game-tile__description {
-  width: 700px;
-  display: flex;
-  height: 100%;
-  padding: 20px 45px 70px 20px;
+  &__description {
+    width: 700px;
+    display: flex;
+    height: 100%;
+    padding: 20px 45px 70px 20px;
+  }
 }
 
 .cart {
   display: flex;
   flex-direction: column;
-  align-items: flex-end;
+  align-items: center;
   justify-content: center;
-  width: 500px;
+  width: 220px;
   height: 250px;
 
   &__price {
     display: flex;
     width: 100%;
-    height: 20%;
-    justify-content: center;
+    height: 50px;
+    justify-content: flex-end;
     align-items: center;
-    padding: 30px 20px 0 15px;
+    margin: 30px 20px 0 15px;
+    padding: 0 20px 0 0;
   }
 
   &__rate {
     display: flex;
-    flex-direction: row;
-    align-items: center;
-    justify-content: center;
+    flex-direction: column;
+    align-items: flex-end;
+    justify-content: flex-start;
     width: 100%;
-    height: 20%;
+    height: 200px;
+    padding: 15px 20px 20px 20px;
+    margin: 0 0 0 0;
 
     &--icon {
       max-width: 30px;
       max-height: 30px;
+    }
+
+    &--stars {
+      display: flex;
+      flex-direction: row;
+    }
+
+    &--reviews {
+      display: flex;
+      justify-content: end;
+      width: 100%;
+      height: 20px;
+      text-align: start;
+      font-size: 1rem;
+      font-family: Inter, serif;
+      font-weight: normal;
+      margin: 5px 0 0 0;
     }
   }
 
@@ -171,7 +194,8 @@ const props = defineProps({
     justify-content: center;
     align-items: center;
     width: 100%;
-    height: 20%;
+    height: 40px;
+    margin: 0 20px 20px 0;
 
     &--default {
       display: flex;
@@ -195,7 +219,7 @@ const props = defineProps({
 
     &--text {
       font-size: 1rem;
-      padding: 5px 10px 5px 10px;
+      margin: 5px 10px 5px 10px;
       font-family: Inter, serif;
       font-weight: bold;
     }
