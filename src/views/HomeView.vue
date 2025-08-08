8744f4cd26b14b149f941855498b7b2a3fa20762
pawelkapusta <pawelllosek@gmail.com>
2025-08-08 12:58:32 +0200

    Rename some classes names using BEM convention, add module to css

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,16 @@
 <template>
-  <div class="home">
-    <TopGamesCarousel :slides="carouselSlides" />
+  <div :class="$style.home">
+    <TopGamesCarousel
+      :slides="carouselSlides"
+      :class="$style['home__games-carousel']"
+    />
+    <GameTile :genres="genre" :class="$style['home__game-tile']" />
   </div>
 </template>
 
 <script setup>
 import TopGamesCarousel from "@/components/TopGamesCarousel.vue";
+import GameTile from "@/components/GameTile.vue";
 
 const carouselSlides = [
   [
@@ -63,14 +68,35 @@ const carouselSlides = [
       imagePath: "Cyberpunk2077.jpg",
       alternativeText: "Image of game Cyberpunk 2077.",
     },
+    {
+      imagePath: "Cyberpunk2077.jpg",
+      alternativeText: "Image of game Cyberpunk 2077.",
+    },
   ],
+];
+
+const genre = [
+  { name: "Adventure" },
+  { name: "RPG" },
+  { name: "CO-OP" },
+  { name: "Horror" },
 ];
 </script>
 
-<style lang="scss" scoped>
+<style module lang="scss">
 .home {
   height: 100%;
   display: flex;
   justify-content: center;
+  align-items: center;
+  flex-direction: column;
+
+  &__games-carousel {
+    padding: 0 10px 10px 10px;
+  }
+
+  &__game-tile {
+    padding: 10px 10px 10px 10px;
+  }
 }
 </style>
