bad6fb8e891495b0d065fade8e0dac39b7d9dc85
pawelkapusta <pawelllosek@gmail.com>
2025-08-08 22:47:37 +0200

    add cart button and price

--- src/components/GameTile.vue
+++ src/components/GameTile.vue
@@ -19,7 +19,7 @@ const props = defineProps({
       :src="require('@/assets/Cyberpunk2077.jpg')"
       :alt="gameTileTitle"
     />
-    <div :class="$style['game-tile_content']">
+    <div :class="$style['game-tile__content']">
       <div :class="$style['game-tile__title']">CYBERPUNK 2077</div>
       <div :class="$style['game-tile__genres']">
         <template v-for="index in genres" :key="index">
@@ -33,8 +33,18 @@ const props = defineProps({
         non porttitor velit... Read more
       </div>
     </div>
-    <div :class="$style['game-tile__end-part']">
-      <div :class="$style['game-tile__cart']">Cart</div>
+    <div :class="[$style.cart, $style['game-tile__cart']]">
+      <h1 :class="[$style['cart__price']]">PLN 159.99</h1>
+      <div :class="$style['cart__button']">
+        <button type="button" :class="$style['cart__button--default']">
+          <img
+            :class="$style['cart__button--icon']"
+            :src="require('@/assets/GameTile/shopping-cart_white.png')"
+            alt="Shopping cart icon"
+          />
+          <h1 :class="$style['cart__button--text']">ADD TO CART</h1>
+        </button>
+      </div>
     </div>
   </div>
 </template>
@@ -50,31 +60,35 @@ const props = defineProps({
   border-radius: 10px;
   background-color: #f5f5f5;
 
-  &__content {
-    display: flex;
-  }
-
   &__image {
     object-fit: cover;
-    width: 214px;
+    max-width: 214px;
     height: 250px;
     border-right: 2px black solid;
   }
+
+  &__content {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  &__cart {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
 }
-.game-tile__content--end--part {
-  width: 25%;
+.game-tile__content--cart {
+  width: 300px;
   height: 100%;
   display: flex;
   justify-content: flex-end;
   align-items: self-end;
-}
-
-.game-tile_content {
-  width: 100%;
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
 }
 
 .game-tile__title {
@@ -112,5 +126,58 @@ const props = defineProps({
   display: flex;
   height: 100%;
   padding: 20px 45px 70px 20px;
+}
+
+.cart {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+  justify-content: center;
+  width: 500px;
+  height: 250px;
+
+  &__price {
+    display: flex;
+    width: 100%;
+    height: 20%;
+    justify-content: center;
+    align-items: center;
+    padding: 30px 20px 0 15px;
+  }
+
+  &__button {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 20%;
+
+    &--default {
+      display: flex;
+      flex-direction: row;
+      justify-content: center;
+      align-items: center;
+      width: 200px;
+      height: 40px;
+      border: none;
+      border-radius: 10px;
+      background-color: #0073e5;
+      color: #f5f5f5;
+    }
+
+    &--icon {
+      object-fit: cover;
+      width: 30px;
+      height: 30px;
+      gap: 20px;
+    }
+
+    &--text {
+      font-size: 1rem;
+      padding: 5px 10px 5px 10px;
+      font-family: Inter, serif;
+      font-weight: bold;
+    }
+  }
 }
 </style>
